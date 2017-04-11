e71d4f1fbf534a2d93c5aa0a62e9115b7dd3d251
flfk <flfk@users.noreply.github.com>
2017-04-12 08:22:25 +1000

    tableView Boilerplate added, Cell swiftfile added, fixed index bug

---
+++ Ten Thousand Hours/GoalCell.swift
@@ -0,0 +1,19 @@
+//
+//  GoalCell.swift
+//  Ten Thousand Hours
+//
+//  Created by Felix Kramer on 12/4/17.
+//  Copyright © 2017 Felix Kramer. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+//XXneed to connnect the IBoutlets from the cell still!
+    
+    func configureCell(item: Goal) {
+        //need to connect the IBoulets to the format required later
+    }
+
+}
--- Ten Thousand Hours/MainVC.swift
+++ Ten Thousand Hours/MainVC.swift
@@ -7,18 +7,65 @@
 //
 
 import UIKit
+//import CoreData
+import CoreData
 
-class MainVC: UIViewController {
+//pull the protocols needed for tables and for core data
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate {
 
+    //create the fetched results controller, and include what data class to work with
+    var controller: NSFetchedResultsController<Goal>!
+    
+    //create the tableview
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //insert the required table delegates
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
     }
+    
+    //Boilerplate code for Table View
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //set the reuse identifier in the storyboard
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GoalCell", for: indexPath) as! GoalCell
+        configureCell(cell: cell, indexPath: indexPath as NSIndexPath)
+        return cell
+    }
+    
+    //note create the following dunction to pass all the goals into the configure cell item help in GoalCell.swift
+    func configureCell(cell: GoalCell, indexPath: NSIndexPath) {
+        let goal = controller.object(at: indexPath as IndexPath)
+        cell.configureCell(item: goal)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //take sections out of the controller
+        if let sections = controller.sections {
+            let sectionInfo = sections[section]
+            return sectionInfo.numberOfObjects
+        }
+        //if none, return 0
+        return 0
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        //count sections from controller
+        if let sections = controller.sections {
+            return sections.count
+        }
+        //if none, return 0
+        return 0
+    }
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    //Boilerplate code for Core Data
+
 
 
 }
